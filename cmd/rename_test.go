5cc1f9c4130b4fb6d95aa6a5d23655492d73dc57
hidez8891 <427357+hidez8891@users.noreply.github.com>
2018-10-28 20:44:10 +0900

    add testcase

--- cmd/rename_test.go
+++ cmd/rename_test.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/hidez8891/zip"
@@ -9,9 +10,9 @@ import (
 
 func TestRenameExecuteOverwrite(t *testing.T) {
 	tests := []struct {
-		file  string
-		args  []string
-		files []string
+		file     string
+		args     []string
+		contents []string
 	}{
 		{
 			file: "../testcase/test.zip",
@@ -23,7 +24,7 @@ func TestRenameExecuteOverwrite(t *testing.T) {
 				"--to",
 				".md",
 			},
-			files: []string{
+			contents: []string{
 				"dir/",
 				"dir/text1.md",
 				"dir/text2.md",
@@ -40,7 +41,7 @@ func TestRenameExecuteOverwrite(t *testing.T) {
 				"--to",
 				"texttext",
 			},
-			files: []string{
+			contents: []string{
 				"dir/",
 				"dir/texttext1.txt",
 				"dir/texttext2.txt",
@@ -59,7 +60,7 @@ func TestRenameExecuteOverwrite(t *testing.T) {
 				"--to",
 				".md",
 			},
-			files: []string{
+			contents: []string{
 				"dir/",
 				"dir/text1.txt",
 				"dir/text2.txt",
@@ -78,7 +79,7 @@ func TestRenameExecuteOverwrite(t *testing.T) {
 				"--to",
 				".md",
 			},
-			files: []string{
+			contents: []string{
 				"dir/",
 				"dir/text1.md",
 				"dir/text2.md",
@@ -95,21 +96,89 @@ func TestRenameExecuteOverwrite(t *testing.T) {
 		defer os.Remove(tmpname)
 
 		helperExecuteCommand(t, append(tt.args, tmpname))
+		helperRenameCheckFileContents(t, tmpname, tt.contents)
+	}
+}
 
-		zr, err := zip.OpenReader(tmpname)
-		if err != nil {
-			t.Fatal(err)
-		}
-		defer zr.Close()
+func TestRenameParallelExecute(t *testing.T) {
+	tests := []struct {
+		files    []string
+		args     []string
+		contents map[string][]string
+	}{
+		{
+			files: []string{
+				"../testcase/test.zip",
+				"../testcase/test2.zip",
+			},
+			args: []string{
+				"rename",
+				"--overwrite",
+				"--from",
+				".txt",
+				"--to",
+				".md",
+				"--jobs=2",
+			},
+			contents: map[string][]string{
+				"../testcase/test.zip": []string{
+					"dir/",
+					"dir/text1.md",
+					"dir/text2.md",
+					"text1.md",
+				},
+				"../testcase/test2.zip": []string{
+					"dir/",
+					"dir/text1.md",
+					"dir/text2.md",
+					"text1.md",
+				},
+			},
+		},
+	}
 
-		if len(zr.File) != len(tt.files) {
-			t.Fatalf("update filename count=%d, want %d", len(zr.File), len(tt.files))
-		}
-
-		for i, zf := range zr.File {
-			if zf.Name != tt.files[i] {
-				t.Fatalf("update filename=%q, want %q", zf.Name, tt.files[i])
+	for _, tt := range tests {
+		tempfilemap := make(map[string]string)
+		for _, filename := range tt.files {
+			tmpname, err := copyTempFile(filename)
+			if err != nil {
+				t.Fatal(err)
 			}
+			tempfilemap[filename] = tmpname
+		}
+		defer func() {
+			for _, tmpname := range tempfilemap {
+				os.Remove(tmpname)
+			}
+		}()
+
+		wildCardPath := filepath.Dir(tempfilemap[tt.files[0]])
+		wildCardPath = filepath.Join(wildCardPath, "*.zip")
+		helperExecuteCommand(t, append(tt.args, wildCardPath))
+
+		for _, filename := range tt.files {
+			tmpname := tempfilemap[filename]
+			contents := tt.contents[filename]
+
+			helperRenameCheckFileContents(t, tmpname, contents)
+		}
+	}
+}
+
+func helperRenameCheckFileContents(t *testing.T, filename string, contents []string) {
+	zr, err := zip.OpenReader(filename)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer zr.Close()
+
+	if len(zr.File) != len(contents) {
+		t.Fatalf("update filename count=%d, want %d", len(zr.File), len(contents))
+	}
+
+	for i, zf := range zr.File {
+		if zf.Name != contents[i] {
+			t.Fatalf("update filename=%q, want %q", zf.Name, contents[i])
 		}
 	}
 }
--- cmd/rm_test.go
+++ cmd/rm_test.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/hidez8891/zip"
@@ -9,9 +10,9 @@ import (
 
 func TestRmExecuteOverwrite(t *testing.T) {
 	tests := []struct {
-		file  string
-		args  []string
-		files []string
+		file     string
+		args     []string
+		contents []string
 	}{
 		{
 			file: "../testcase/test.zip",
@@ -21,7 +22,7 @@ func TestRmExecuteOverwrite(t *testing.T) {
 				"--filter",
 				"text1.txt",
 			},
-			files: []string{
+			contents: []string{
 				"dir/",
 				"dir/text1.txt",
 				"dir/text2.txt",
@@ -35,7 +36,7 @@ func TestRmExecuteOverwrite(t *testing.T) {
 				"--filter",
 				"*.txt",
 			},
-			files: []string{
+			contents: []string{
 				"dir/",
 				"dir/text1.txt",
 				"dir/text2.txt",
@@ -49,7 +50,7 @@ func TestRmExecuteOverwrite(t *testing.T) {
 				"--regexp",
 				"\\d.txt",
 			},
-			files: []string{
+			contents: []string{
 				"dir/",
 			},
 		},
@@ -63,21 +64,67 @@ func TestRmExecuteOverwrite(t *testing.T) {
 		defer os.Remove(tmpname)
 
 		helperExecuteCommand(t, append(tt.args, tmpname))
+		helperRmCheckFileContents(t, tmpname, tt.contents)
+	}
+}
 
-		zr, err := zip.OpenReader(tmpname)
-		if err != nil {
-			t.Fatal(err)
-		}
-		defer zr.Close()
+func TestRmParallelExecute(t *testing.T) {
+	tests := []struct {
+		files    []string
+		args     []string
+		contents map[string][]string
+	}{
+		{
+			files: []string{
+				"../testcase/test.zip",
+				"../testcase/test2.zip",
+			},
+			args: []string{
+				"rm",
+				"--overwrite",
+				"--filter",
+				"*.txt",
+				"--jobs=2",
+			},
+			contents: map[string][]string{
+				"../testcase/test.zip": []string{
+					"dir/",
+					"dir/text1.txt",
+					"dir/text2.txt",
+				},
+				"../testcase/test2.zip": []string{
+					"dir/",
+					"dir/text1.txt",
+					"dir/text2.txt",
+				},
+			},
+		},
+	}
 
-		if len(zr.File) != len(tt.files) {
-			t.Fatalf("update filename count=%d, want %d", len(zr.File), len(tt.files))
-		}
-
-		for i, zf := range zr.File {
-			if zf.Name != tt.files[i] {
-				t.Fatalf("update filename=%q, want %q", zf.Name, tt.files[i])
+	for _, tt := range tests {
+		tempfilemap := make(map[string]string)
+		for _, filename := range tt.files {
+			tmpname, err := copyTempFile(filename)
+			if err != nil {
+				t.Fatal(err)
 			}
+			tempfilemap[filename] = tmpname
+		}
+		defer func() {
+			for _, tmpname := range tempfilemap {
+				os.Remove(tmpname)
+			}
+		}()
+
+		wildCardPath := filepath.Dir(tempfilemap[tt.files[0]])
+		wildCardPath = filepath.Join(wildCardPath, "*.zip")
+		helperExecuteCommand(t, append(tt.args, wildCardPath))
+
+		for _, filename := range tt.files {
+			tmpname := tempfilemap[filename]
+			contents := tt.contents[filename]
+
+			helperRmCheckFileContents(t, tmpname, contents)
 		}
 	}
 }
@@ -113,5 +160,23 @@ func TestRmNotModified(t *testing.T) {
 
 	if time1 != time2 {
 		t.Fatalf("file was changed unnecessarily")
+	}
+}
+
+func helperRmCheckFileContents(t *testing.T, filename string, contents []string) {
+	zr, err := zip.OpenReader(filename)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer zr.Close()
+
+	if len(zr.File) != len(contents) {
+		t.Fatalf("update filename count=%d, want %d", len(zr.File), len(contents))
+	}
+
+	for i, zf := range zr.File {
+		if zf.Name != contents[i] {
+			t.Fatalf("update filename=%q, want %q", zf.Name, contents[i])
+		}
 	}
 }
