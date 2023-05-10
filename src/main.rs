d44cccf29b9d5978dc3108dbfd27445b1c36cfde
hidez8891 <427357+hidez8891@users.noreply.github.com>
2023-05-10 22:33:24 +0900

    add: ls command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+use std::fs::File;
+
+use clap::{Parser, Subcommand};
+use zip;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// List zip-archive contents
+    Ls {
+        /// zip-archive path(s)
+        path: Vec<String>,
+    },
+}
+
 fn main() {
-    println!("Hello, rust ziped!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Ls { path } => {
+            for filepath in path {
+                exec_list(filepath).expect("failed");
+            }
+        }
+    }
+}
+
+fn exec_list(path: &String) -> zip::result::ZipResult<()> {
+    let reader = File::open(path)?;
+    let mut zip = zip::ZipArchive::new(reader)?;
+
+    for i in 0..zip.len() {
+        let file = zip.by_index(i)?;
+        println!("{}", file.name());
+    }
+
+    Ok(())
 }
